3411a2782a1ca2bd4a952a1fa46b53b6662ad943
shadow <lightshadow416@gmail.com>
2023-11-26 21:30:29 +0100

    adding new reducer file [toggle_taskPanel] , not complete

--- components/SelectedTask_panel/SelectedTask_panel.jsx
+++ components/SelectedTask_panel/SelectedTask_panel.jsx
@@ -1,4 +1,14 @@
+"use client";
+// !redux
+import { useDispatch, useSelector } from "react-redux";
 import "./selectedtask_panel.css";
 export default function SelectedTask_panel() {
-  return <>SelectedTask_panel</>;
+  return (
+    <>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
 }
